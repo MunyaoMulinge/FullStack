e798a5b675db281f2fb99fe936a373a1732de931
Victor Munyao <victor.mulinge@tangazoletu.com>
2024-09-01 21:33:09 +0300

    CRUD-DeleteAndUpdate

--- src/main/java/com/munyaocodes/customer/CustomerService.java
+++ src/main/java/com/munyaocodes/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package com.munyaocodes.customer;
 
 import com.munyaocodes.exception.DuplicateResourceException;
+import com.munyaocodes.exception.RequestValidationException;
 import com.munyaocodes.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -42,5 +43,39 @@ public class CustomerService {
                 customerRegistrationRequest.age()
         );
         customerDao.insertCustomer(customer);
+    }
+    public void deleteCustomerById(Integer customerId) {
+        if(!customerDao.existsPersonWithId(customerId)){
+            throw new ResourceNotFoundException(
+                    "customer with id [%s] not found".formatted(customerId)
+            );
+        }
+        customerDao.deleteCustomerById(customerId);
+    }
+    public void updateCustomer(Integer customerId,
+                               CustomerUpdateRequest updateRequest){
+        Customer customer = getCustomer(customerId);
+        boolean changes = false;
+        if (updateRequest.name() != null && updateRequest.name().equals(customer.getName())){
+            customer.setName(updateRequest.name());
+            changes = true;
+        }
+        if (updateRequest.age() != null && updateRequest.age().equals(customer.getAge())){
+            customer.setAge(updateRequest.age());
+            changes = true;
+        }
+        if (updateRequest.email() != null && updateRequest.email().equals(customer.getEmail())){
+            if (customerDao.existsPersonWithEmail(updateRequest.email())){
+                throw new DuplicateResourceException(
+                        "email already taken"
+                );
+            }
+            customer.setEmail(updateRequest.email());
+            changes = true;
+        }
+        if(!changes){
+            throw new RequestValidationException("no data changes found");
+        }
+        customerDao.updateCustomer(customer);
     }
 }
---
+++ src/main/java/com/munyaocodes/customer/CustomerUpdateRequest.java
@@ -0,0 +1,9 @@
+package com.munyaocodes.customer;
+
+public record CustomerUpdateRequest(
+    String name,
+    String email,
+    Integer age
+) {
+
+}
