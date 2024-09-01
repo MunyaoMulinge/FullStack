ee64972017e00c1a3e65492d0ac9fce01c64da32
Victor Munyao <victor.mulinge@tangazoletu.com>
2024-09-01 11:37:34 +0300

    changes

---
+++ src/main/java/com/munyaocodes/Main.java
@@ -0,0 +1,42 @@
+package com.munyaocodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/greet")
+    public GreetResponse greet(
+            @RequestParam(value = "name", required = false) String name) {
+        String greetMessage = name == null || name.isBlank() ?
+                "Hello" : "Hello " + name;
+
+        GreetResponse response = new GreetResponse(
+                greetMessage,
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Alex", 28, 30_000)
+        );
+
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {
+    }
+}
