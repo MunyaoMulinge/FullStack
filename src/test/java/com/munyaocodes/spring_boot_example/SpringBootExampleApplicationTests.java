5886df18b801883b3eeb391dd506260a3f1ad5c5
Victor Munyao <victor.mulinge@tangazoletu.com>
2024-09-01 10:41:05 +0300

    first commit

---
+++ src/test/java/com/munyaocodes/spring_boot_example/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munyaocodes.spring_boot_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
