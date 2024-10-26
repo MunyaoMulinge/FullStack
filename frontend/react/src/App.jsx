501e03ddb26de2b1ac4d8ea45b042befeb218b5a
victor.munyao <victor.mulinge@tangazoletu.com>
2024-10-26 13:51:04 +0300

    frontEndUI

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -3,21 +3,31 @@ import SidebarWithHeader from './components/shared/SideBar.jsx'
 import { useEffect, useState } from 'react'
 import { getCustomers } from "./services/client.js"
 import CardWithImage from "./components/Card"
+import DrawerForm from "./components/DrawerForm.jsx";
+import {errorNotification} from "./services/notification.js";
 
 const App = () => {
 
     const [customers, setCustomers] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [err, setError] = useState("");
 
-    useEffect(() => {
+    const fetchCustomers = () => {
         setLoading(true)
         getCustomers().then(res => {
-                setCustomers(res.data)
+            setCustomers(res.data)
         }).catch(err => {
-            console.log(err)
+            setError(err.response.data.message)
+            errorNotification(
+                err.code,
+                err.response.data.message
+            )
         }).finally(() => {
             setLoading(false)
         })
+    }
+    useEffect(() => {
+        fetchCustomers()
     }, []);
 
     if (loading) {
@@ -33,17 +43,32 @@ const App = () => {
             </SidebarWithHeader>
         )
     }
-
+    if(err){
+        return (
+            <SidebarWithHeader>
+                <DrawerForm
+                    fetchCustomers={fetchCustomers}
+                />
+                <Text mt={5}>There was an error</Text>
+            </SidebarWithHeader>
+        )
+    }
     if(customers.length <= 0){
         return (
             <SidebarWithHeader>
-                <Text>No customers available</Text>
+                <DrawerForm
+                    fetchCustomers={fetchCustomers}
+                />
+                <Text mt={5}>No customers available</Text>
             </SidebarWithHeader>
         )
     }
     return (
         <SidebarWithHeader>
-            <Wrap justify='center' spacing={30}>
+            <DrawerForm
+                fetchCustomers={fetchCustomers}
+            />
+            <Wrap justify='center' spacing={"30px"}>
                 {customers.map((customer, index) => (
                     <WrapItem key={index}>
                         <CardWithImage
